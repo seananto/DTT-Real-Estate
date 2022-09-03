0d53a85c9a49e5a430379a98b847442234275c86
Sato <sato@satos-air.home>
2022-09-03 19:28:52 +0200

    first commit

---
+++ src/main/java/com/example/dtt_real_estate/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dtt_real_estate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // hig
+    }
+}

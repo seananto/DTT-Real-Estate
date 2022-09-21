80375f5971de3110bd41d54c9cbc316e9b0a0d20
Sato <sato@satos-air.home>
2022-09-21 17:57:17 +0200

    Added an about page activity and made a simple design

---
+++ src/main/java/com/example/dtt_real_estate/AboutPageActivity.java
@@ -0,0 +1,17 @@
+package com.example.dtt_real_estate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+
+        // design the about page
+
+    }
+}

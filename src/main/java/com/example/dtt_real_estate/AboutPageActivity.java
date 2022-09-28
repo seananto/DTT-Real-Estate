ede47d8236d26e14851fd96679d038b418581e78
Sato <sato@satos-air.home>
2022-09-28 12:00:18 +0200

    added hyperlink

--- src/main/java/com/example/dtt_real_estate/AboutPageActivity.java
+++ src/main/java/com/example/dtt_real_estate/AboutPageActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.text.method.LinkMovementMethod;
 
 public class AboutPageActivity extends AppCompatActivity {
-    // declare variables
+    // declare variables, views
     ImageView toolbar;
     ImageView homeButton;
     ImageView infoButton;
+    TextView hyperlink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,10 @@ public class AboutPageActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.iv_toolbar);
         homeButton = findViewById(R.id.iv_home_button);
         infoButton = findViewById(R.id.iv_info_button);
+        hyperlink = findViewById(R.id.tv_hyperlink);
+
+        // method that takes user to dtt website
+        setupHyperLink();
 
         // set onClickListener to handle click events with buttons
         homeButton.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +48,11 @@ public class AboutPageActivity extends AppCompatActivity {
                 SwitchToAboutPageActivity();
             }
         });
+
+
     }
+
+    // methods
     private void SwitchToOverviewActivity(){
         Intent switchActivityIntent = new Intent(this, OverviewActivity.class);
         startActivity(switchActivityIntent);
@@ -51,6 +63,11 @@ public class AboutPageActivity extends AppCompatActivity {
         Intent switchActivityIntent = new Intent(this, AboutPageActivity.class);
         startActivity(switchActivityIntent);
         finish();
+    }
+
+    // make the hyperlink clickable and once clicked takes the user to the website in browser
+    private void setupHyperLink(){
+        hyperlink.setMovementMethod(LinkMovementMethod.getInstance() );
     }
 }
 
